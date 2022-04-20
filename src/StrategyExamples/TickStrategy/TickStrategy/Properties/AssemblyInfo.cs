0e8d4e406a94ed37bc1f5d1bb491f08cbbc81596
Jeff Wobbe <jwobbe@gmail.com>
2022-04-20 14:00:44 -0700

    Tick strategy example

---
+++ src/StrategyExamples/TickStrategy/TickStrategy/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿// Copyright QUANTOWER LLC. © 2017-2021. All rights reserved.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TickStrategy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TickStrategy")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("51c33655-3074-427d-993e-fc072abe5382")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/StrategyExamples/TickStrategy/TickStrategy/TickStrategy.cs
@@ -0,0 +1,133 @@
+﻿// Copyright QUANTOWER LLC. © 2017-2021. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using TradingPlatform.BusinessLayer;
+
+namespace TickStrategy
+{
+    /// <summary>
+    /// An example of strategy for working with one symbol. Add your code, compile it and run via Strategy Runner panel in the assigned trading terminal.
+    /// Information about API you can find here: http://api.quantower.com
+    /// </summary>
+    public class TickStrategy : Strategy, ICurrentSymbol
+    {
+        private HistoricalData _historicalData;
+
+        [InputParameter("Symbol", 10)]
+        public Symbol CurrentSymbol { get; set; }
+
+        [InputParameter("Account", 20)]
+        public Account Account { get; set; }
+
+        [InputParameter("Tick Count", 20)]
+        public int TickCount { get; set; } = 233;
+
+
+        public override string[] MonitoringConnectionsIds => new string[] { this.CurrentSymbol?.ConnectionId };
+
+        public TickStrategy()
+          : base()
+        {
+            // Defines strategy's name and description.
+            this.Name = "TickStrategy";
+            this.Description = "My strategy's annotation";
+        }
+
+        /// <summary>
+        /// This function will be called after creating a strategy
+        /// </summary>
+        protected override void OnCreated()
+        {
+            // Add your code here
+        }
+
+        /// <summary>
+        /// This function will be called after running a strategy
+        /// </summary>
+        protected override void OnRun()
+        {
+            if (CurrentSymbol == null || Account == null || CurrentSymbol.ConnectionId != Account.ConnectionId)
+            {
+                Log("Incorrect input parameters... Symbol or Account are not specified or they have diffent connectionID.", StrategyLoggingLevel.Error);
+                return;
+            }
+
+            CurrentSymbol = Core.GetSymbol(this.CurrentSymbol?.CreateInfo());
+            var aggregation = new HistoryAggregationTick(TickCount);
+            var fromTime = FindFuturesWeekStartBefore(Core.TimeUtils.DateTimeUtcNow);
+            Log($"History starting at {fromTime}");
+
+            _historicalData = CurrentSymbol.GetHistory(new HistoryRequestParameters
+            {
+                Aggregation = aggregation,
+                FromTime = fromTime,
+                HistoryType = HistoryType.Last,
+                UsePrevCloseAsOpenPriceBar = false,
+            });
+
+            _historicalData.NewHistoryItem += HistoricalData_NewHistoryItem;
+            _historicalData.HistoryItemUpdated += HistoricalData_HistoryItemUpdated;
+        }
+
+        private void HistoricalData_HistoryItemUpdated(object sender, HistoryEventArgs e)
+        {
+            //Log($"Ticks Remaining: {TickCount - e.HistoryItem[PriceType.Ticks]}");
+        }
+
+        /// <summary>
+        /// This function will be called after stopping a strategy
+        /// </summary>
+        protected override void OnStop()
+        {
+            if (_historicalData != null)
+            {
+                _historicalData.NewHistoryItem -= HistoricalData_NewHistoryItem;
+                _historicalData.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// This function will be called after removing a strategy
+        /// </summary>
+        protected override void OnRemove()
+        {
+            this.CurrentSymbol = null;
+            this.Account = null;
+            // Add your code here
+        }
+
+        /// <summary>
+        /// Use this method to provide run time information about your strategy. You will see it in StrategyRunner panel in trading terminal
+        /// </summary>
+        protected override List<StrategyMetric> OnGetMetrics()
+        {
+            List<StrategyMetric> result = base.OnGetMetrics();
+
+            // An example of adding custom strategy metrics:
+            // result.Add("Opened buy orders", "2");
+            // result.Add("Opened sell orders", "7");
+
+            return result;
+        }
+
+        private DateTime FindFuturesWeekStartBefore(DateTime dateTime)
+        {
+            var weeklyStartDate = dateTime;
+            while (weeklyStartDate.DayOfWeek != DayOfWeek.Sunday)
+            {
+                weeklyStartDate = weeklyStartDate.AddDays(-1);
+            }
+
+            weeklyStartDate = new DateTime(weeklyStartDate.Year, weeklyStartDate.Month, weeklyStartDate.Day, 17, 0, 0);
+            var futuresExchangeTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
+            var weeklyStartDateOffsetFuturesExchange = new DateTimeOffset(weeklyStartDate, futuresExchangeTimeZone.GetUtcOffset(weeklyStartDate));
+            return weeklyStartDateOffsetFuturesExchange.UtcDateTime;
+        }
+
+        private void HistoricalData_NewHistoryItem(object sender, HistoryEventArgs e)
+        {
+            Log($"New candle started at {DateTime.Now} with an open price of {e.HistoryItem[PriceType.Open]}");
+        }
+    }
+}
